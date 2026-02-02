f3e1407121e4ecb2bb296d269ecf64b1d88bd7e4
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-02-03 02:10:33 +0600

    Update CORS configuration to allow PATCH method

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(
   cors({
     origin:["http://localhost:5173"], 
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
